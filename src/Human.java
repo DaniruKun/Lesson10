1ec8f5e96e8063369911476f20ec9707e57bdac4
Daniil Petrov <dan181818@gmail.com>
2016-04-01 19:39:56 +0300

    Added new Human class

---
+++ src/Human.java
@@ -0,0 +1,5 @@
+
+public class Human {
+	static String name;
+	static int age;
+}
--- src/PatternTest.java
+++ src/PatternTest.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 
 public class PatternTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		sout("Please enter your age");
+		Scanner scanner = new Scanner(System.in);
+		Human human = new Human();
+		int age = scanner.nextInt(); //pattern scan for integer
+		if (age < 18) {
+			sout("Access is denied");
+		}
+		else {
+			human.age = age;
+			sout("Please enter your name");
+			human.name = scanner.nextLine();
+			
+		}
+		getID();
+		
+	}
+	public static void sout(String s) {
+		System.out.println(s);
+	}
+	
+	public static void getID() {
+		System.out.print("Name:");
+		sout(Human.name);
+		System.out.print("Age:");
+		sout(Integer.toString(Human.age));
+		
+		
 	}
 
 }
